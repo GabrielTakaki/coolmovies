1d7d5a47cb56c38cc0950854877016bb79cb7206
gabrieltakaki <gabitj@icloud.com>
2024-08-20 15:48:52 -0300

    refactor: variant options of the text

--- src/components/data-display/Text.tsx
+++ src/components/data-display/Text.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography } from "@mui/material";
 
 type TextProps = {
-  variant?: "h1" | "h2" | "h3" | "h4" | "caption" | "body1" | "subtitle1";
+  variant?: "h6" | "caption" | "body1" | "subtitle1";
   align?: "left" | "center" | "right";
   children?: React.ReactNode;
 };
