450f255929ed9c773611e0860bd61593582ac7a4
gabrieltakaki <gabitj@icloud.com>
2024-08-20 15:13:18 -0300

    feat: it overrides the mui default color pallet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,48 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import React, { FC, useState } from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import Head from 'next/head';
-import { createStore } from '../redux/store';
-import { EnhancedStore } from '@reduxjs/toolkit';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React, { FC, useState } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import Head from "next/head";
+import { createStore } from "../redux/store";
+import { EnhancedStore } from "@reduxjs/toolkit";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { COLOR_TOKENS } from "../consts/design-system/global-tokens/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: COLOR_TOKENS.primary,
+    secondary: COLOR_TOKENS.secondary,
+    info: COLOR_TOKENS.info,
+  },
+});
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   const [store, setStore] = useState<EnhancedStore | null>(null);
+
   React.useEffect(() => {
     const client = new ApolloClient({
       cache: new InMemoryCache(),
-      uri: '/graphql',
+      uri: "/graphql",
     });
 
     const store = createStore({ epicDependencies: { client } });
     setStore(store);
   }, []);
-  if (!store) return <>{'Loading...'}</>;
+
+  if (!store) return <>{"Loading..."}</>;
   return (
     <>
       <Head>
-        <title>{'Coolmovies Frontend'}</title>
-        <meta charSet='UTF-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <title>{"Coolmovies Frontend"}</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <ReduxProvider store={store}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </ReduxProvider>
     </>
   );
