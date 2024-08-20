0ea6bb83a4b306b15e3a70d1e355ef8ebabc2cc0
gabrieltakaki <gabitj@icloud.com>
2024-08-20 15:14:03 -0300

    feat: Text component

--- src/components/data-display/Text.tsx
+++ src/components/data-display/Text.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Typography } from "@mui/material";
 
-function Text(props) {
-    return (
-        <div></div>
-    );
+type TextProps = {
+  variant?: "h1" | "h2" | "h3" | "h4" | "caption" | "body1" | "subtitle1";
+  align?: "left" | "center" | "right";
+  children?: React.ReactNode;
+};
+
+function Text({ variant = "body1", align = "left", children }: TextProps) {
+  return (
+    <Typography variant={variant} align={align}>
+      {children}
+    </Typography>
+  );
 }
 
 export default Text;
