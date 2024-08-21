1a56d8546d5250ba0f4f4c79776d4c3fa9de10ac
gabrieltakaki <gabitj@icloud.com>
2024-08-21 13:48:38 -0300

    fix: it fetches the data in the layout instead of in the index of reviews

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./general/Header";
 import Box from "./layout/Box";
 import useMediaQuery from "../hooks/useMediaQuery";
+import { moviesActions, useAppDispatch } from "../redux/store";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const dispatch = useAppDispatch();
   const isDesktop = useMediaQuery("md");
+
+  useEffect(() => {
+    dispatch(moviesActions.fetch());
+  }, [dispatch]);
   return (
     <>
       <Header />
--- src/pages/reviews/index.tsx
+++ src/pages/reviews/index.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect } from "react";
-import { moviesActions, useAppDispatch } from "../../redux/store";
+import React from "react";
 import Text from "../../components/data-display/Text";
 import FlexColumn from "../../components/layout/FlexColumn";
 import MoviesList from "./components/MoviesList";
 
 function Reviews() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(moviesActions.fetch());
-  }, []);
-
   return (
     <FlexColumn component="section">
       <Text variant="h6">Reviews</Text>
