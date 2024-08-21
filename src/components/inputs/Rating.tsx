bc96f782943db25d0b53036f47ce373ef4cc2453
gabrieltakaki <gabitj@icloud.com>
2024-08-21 12:57:56 -0300

    feat: Rating.tsx component

---
+++ src/components/inputs/Rating.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Rating as MuiRating, RatingProps } from "@mui/material";
+import Box from "../layout/Box";
+import { SIZES } from "../../consts/design-system/definitions";
+
+type Props = {
+  readOnly?: boolean;
+  value: number | null;
+  disabled?: boolean;
+  onChange?: RatingProps["onChange"];
+  precision?: RatingProps["precision"];
+  size?: SIZES;
+};
+
+function Rating(props: Props) {
+  return (
+    <Box
+      component="div"
+      sx={{
+        "& > legend": { mt: 2 },
+      }}
+    >
+      <MuiRating {...props} />
+    </Box>
+  );
+}
+
+export default Rating;
