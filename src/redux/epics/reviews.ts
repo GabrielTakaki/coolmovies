54cc792d25b3c68a2872df58ff48646201d16d3a
gabrieltakaki <gabitj@icloud.com>
2024-08-21 15:54:23 -0300

    refactor: creates an epic to handle the reviews

---
+++ src/redux/epics/reviews.ts
@@ -0,0 +1,52 @@
+import { Epic } from "redux-observable";
+import { Observable } from "rxjs";
+import { filter, switchMap } from "rxjs/operators";
+import { reviewsActions, SliceAction } from "../slices/reviewsSlice";
+import { getReviews } from "../../services/reviews/getReviews";
+import { updateReview } from "../../services/reviews/updateReview";
+import { createReview } from "../../services/reviews/createReview";
+
+export const fetchReviewsApic: Epic = (
+  action$: Observable<SliceAction["fetchReviews"]>
+) =>
+  action$.pipe(
+    filter(reviewsActions.fetchReviews.match),
+    switchMap(async () => {
+      try {
+        const result = await getReviews();
+        return reviewsActions.setReviews(result);
+      } catch (err) {
+        // return reviewsActions.loadError();
+      }
+    })
+  );
+
+export const createReviewEpic: Epic = (
+  action$: Observable<SliceAction["createReview"]>
+) =>
+  action$.pipe(
+    filter(reviewsActions.createReview.match),
+    switchMap(async (action) => {
+      try {
+        const review = await createReview(action.payload);
+        return reviewsActions.insertOne(review);
+      } catch (err) {
+        // return reviewsActions.loadError();
+      }
+    })
+  );
+
+export const updateReviewEpic: Epic = (
+  action$: Observable<SliceAction["updateReview"]>
+) =>
+  action$.pipe(
+    filter(reviewsActions.updateReview.match),
+    switchMap(async (action) => {
+      try {
+        const review = await updateReview(action.payload);
+        return reviewsActions.mergeEntities(review);
+      } catch (err) {
+        // return reviewsActions.loadError();
+      }
+    })
+  );
