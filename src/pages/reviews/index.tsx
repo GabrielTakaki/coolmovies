b9bb6a3e2b6fb984771edf7beb7080bb083164ba
gabrieltakaki <gabitj@icloud.com>
2024-08-21 12:59:34 -0300

    feat: initial reviews page

--- src/pages/reviews/index.tsx
+++ src/pages/reviews/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { moviesActions, useAppDispatch } from "../../redux/store";
+import Text from "../../components/data-display/Text";
+import FlexColumn from "../../components/layout/FlexColumn";
+import FlexRow from "../../components/layout/FlexRow";
+import Button from "../../components/inputs/Button";
+import MoviesList from "./components/MoviesList";
 
-function Reviews(props) {
-    return (
-        <div>I am the reviews screen</div>
-    );
+function Reviews() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(moviesActions.fetch());
+  }, []);
+
+  return (
+    <FlexColumn component="section">
+      <FlexRow justifyContent="space-between" component="section">
+        <Text variant="h6">Reviews</Text>
+        <Button label="Add Review" />
+      </FlexRow>
+      <MoviesList />
+    </FlexColumn>
+  );
 }
 
 export default Reviews;
