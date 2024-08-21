f47ab9238a7ca375e9411c48376e6e48b0d95b10
gabrieltakaki <gabitj@icloud.com>
2024-08-21 17:33:48 -0300

    fix: createReview service with typing and correct mapping to the graphql

--- src/services/reviews/createReview.ts
+++ src/services/reviews/createReview.ts
@@ -1,23 +1,32 @@
 import { gql } from "@apollo/client";
 import { client } from "../../core/apolloClient";
+import { Review } from "../../models/Review";
 
 const CREATE_REVIEW = gql`
-  mutation CreateReview($input: CreateReviewInput!) {
-    createReview(input: $input) {
-      id
-      title
-      body
-      rating
-      userReviewerId
-      movieId
+  mutation CreateReview($input: CreateMovieReviewInput!) {
+    createMovieReview(input: $input) {
+      movieReview {
+        id
+        title
+        body
+        rating
+        userReviewerId
+        movieId
+      }
     }
   }
 `;
 
-export const createReview = async (input: any) => {
+export const createReview = async (
+  input: Pick<
+    Review,
+    "movieId" | "body" | "title" | "userReviewerId" | "rating"
+  >
+) => {
   const result = await client.mutate({
     mutation: CREATE_REVIEW,
-    variables: { input },
+    variables: { input: { movieReview: input } },
   });
-  return result.data.createReview;
+
+  return result.data.createMovieReview.movieReview;
 };
