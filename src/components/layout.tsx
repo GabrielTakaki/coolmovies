6e284ba1d5fc43e27cbed50f66a14f7c2d12e1cc
gabrieltakaki <gabitj@icloud.com>
2024-08-21 13:00:05 -0300

    refactor: it creates padding for the content wrapped in the layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./general/Header";
+import Box from "./layout/Box";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const isDesktop = useMediaQuery("md");
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <Box component="main" margin={isDesktop ? "20px 160px" : "20px 40px"}>
+        {children}
+      </Box>
     </>
   );
 }
