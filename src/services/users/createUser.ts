41b753a218932a9378a0da01d250fde9dbd84c6e
gabrieltakaki <gabitj@icloud.com>
2024-08-21 17:33:57 -0300

    feat: createUser service

---
+++ src/services/users/createUser.ts
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+import { client } from "../../core/apolloClient";
+
+const CREATE_USER = gql`
+  mutation CreateUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const createUser = async (input: { name: string }) => {
+  const result = await client.mutate({
+    mutation: CREATE_USER,
+    variables: { input: { user: input } },
+  });
+
+  if (result.errors) {
+    throw new Error("Failed to create user");
+  }
+
+  return result.data.createUser.user;
+};
