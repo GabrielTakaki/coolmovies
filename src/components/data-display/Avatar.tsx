c0003fd5bd6aa93e8b14814d61d1bffa23f50746
gabrieltakaki <gabitj@icloud.com>
2024-08-21 13:01:29 -0300

    feat: Avatar component

---
+++ src/components/data-display/Avatar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Avatar as MuiAvatar } from "@mui/material";
+import { SIZES } from "../../consts/design-system/definitions";
+
+function Avatar({
+  src,
+  alt,
+  size = "small",
+}: {
+  src?: string;
+  alt: string;
+  size?: SIZES;
+}) {
+  return (
+    <MuiAvatar
+      alt={alt}
+      src={src || ""}
+      sx={
+        {
+          small: { width: 24, height: 24 },
+          medium: { width: 34, height: 34 },
+          large: { width: 56, height: 56 },
+        }[size]
+      }
+    />
+  );
+}
+
+export default Avatar;
