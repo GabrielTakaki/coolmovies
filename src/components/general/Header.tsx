c4afb1c69cd08405c55d8d9abba8213b091c3b3f
gabrieltakaki <gabitj@icloud.com>
2024-08-21 21:05:08 -0300

    fix: it goes back instead of using the href so the page does not reload when navigating

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useCallback } from "react";
 import FlexRow from "../layout/FlexRow";
 import AnimationIcon from "@mui/icons-material/Animation";
 import { SPACINGS } from "../../consts/design-system/global-tokens/spacings";
 import Link from "../navigation/Link";
+import { useRouter } from "next/router";
 
 function Header() {
+  const router = useRouter();
+  const handleClick = useCallback(() => {
+    router.back();
+  }, []);
+
   return (
     <FlexRow
       component="header"
@@ -15,7 +21,7 @@ function Header() {
       gap={SPACINGS.sm}
     >
       <AnimationIcon />
-      <Link href="/reviews" underline="none" variant="h6">
+      <Link onClick={handleClick} underline="none" variant="h6">
         Coolmovies
       </Link>
     </FlexRow>
