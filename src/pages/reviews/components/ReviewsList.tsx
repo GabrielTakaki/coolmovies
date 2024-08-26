a87ab917911801e6ca803e8ad59bf596dc2b8a64
gabrieltakaki <gabitj@icloud.com>
2024-08-26 13:51:11 -0300

    fix: remove useless button

--- src/pages/reviews/components/ReviewsList.tsx
+++ src/pages/reviews/components/ReviewsList.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import List from "../../../components/data-display/List";
 import { useAppSelector } from "../../../redux/store";
-import Button from "../../../components/inputs/Button";
 import ReviewsListItem from "./ReviewListItem";
 import { selectReviewIds } from "../../../redux/slices/reviewsSlice";
 
@@ -13,14 +12,6 @@ function ReviewList() {
       keyExtractor={(item) => item.toString()}
       renderItem={(item) => <ReviewsListItem reviewId={item.toString()} />}
       data={reviewIds}
-      renderActions={(item) => (
-        <Button
-          variant="outlined"
-          label="Edit"
-          color="secondary"
-          onClick={() => console.log(item)}
-        />
-      )}
     />
   );
 }
